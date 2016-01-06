f9c302be7b0e628c94c7945ae9ba25f8881a81f3
Paul Sheridan <paul.sheridan@me.com>
2016-01-05 17:59:17 -0800

    attempted to filter entire site, failed

--- class-02-jquery-and-events/pair-assignment/starter-codez/scripts/articleView.js
+++ class-02-jquery-and-events/pair-assignment/starter-codez/scripts/articleView.js
@@ -80,13 +80,7 @@ articleView.handleMainNav = function() {
   //       single .tab-content section that is associated with the clicked .tab element.
   //       So: You need to dynamically build a selector string with the correct ID, based on the
   //       data available to you on the .tab element that was clicked.
-  $('.main-nav').on('change', function() {
-    if ($(this).val()) {
-      $('article').hide();
-      var $authorName = $(this).val();
-      $('article[data-author = "'+ $authorName + '"]').fadeIn();
-    }
-  });
+  $('.main-nav').on();
 
   $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
 };
@@ -106,4 +100,5 @@ $(function(){
   articleView.populateFilters();
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
+  articleView.handleMainNav();
 });
