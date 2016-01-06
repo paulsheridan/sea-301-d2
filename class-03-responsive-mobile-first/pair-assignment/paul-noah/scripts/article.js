f1a893366144db8a233a3ac889d1b40946ea5010
Paul Sheridan <paul.sheridan@me.com>
2016-01-06 15:24:29 -0800

    pair assignment day 3 complete

--- class-03-responsive-mobile-first/pair-assignment/paul-noah/scripts/article.js
+++ class-03-responsive-mobile-first/pair-assignment/paul-noah/scripts/article.js
@@ -2,26 +2,30 @@ var articles = [];
 
 function Article (opts) {
   this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
   this.title = opts.title;
   this.category = opts.category;
-  this.authorUrl = opts.authorUrl;
-  this.publishedOn = opts.publishedOn;
   this.body = opts.body;
+  this.publishedOn = opts.publishedOn;
 }
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
-
-  $newArticle.find('title', this.title);
-
-  // Include the publication date as a 'title' attribute to show on hover:
-  $newArticle.find('time[pubdate]').attr('title', this.publishedOn)
-
-  // Display the date as a relative number of "days ago":
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
-
-  $newArticle.append('<hr>');
   $newArticle.removeClass('template');
+  if (!this.publishedOn) {
+    $newArticle.addClass('draft');
+  }
+  $newArticle.attr('data-category', this.category);
+  $newArticle.attr('data-author', this.author)
+
+  $newArticle.find('.byline a').html(this.author);
+  $newArticle.find('.byline a').attr('href', this.authorUrl);
+  $newArticle.find('h1:first').html(this.title);
+  $newArticle.find('.article-body').html(this.body);
+  $newArticle.find('time[pubdate]').attr('datetime', this.publishedOn)
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn)
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
+  $newArticle.append('<hr>');
   return $newArticle;
 }
 
